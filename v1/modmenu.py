2902d24bef9a48d1f73308f7818234b26a1fc3ce
living Gamer <keshabpaul45@gmail.com>
2021-12-02 18:03:15 +0530

    Improved ModMenu

--- v1/modmenu.py
+++ v1/modmenu.py
@@ -1,18 +1,14 @@
-import os
-srcfile = input("Enter your asset_indexer location")
-mods = [r"E:\Hack\Non Cheat Table\FreeFire\Test\head.png", r"E:\Hack\Non Cheat Table\FreeFire\Test\antena.png",r"E:\Hack\Non Cheat Table\FreeFire\Test\default.png"]
+import shutil
+src = input("Enter your asset_indexer location: ")
+mods = {"A" : input("Enter the location of the file in folder antena!"), "H": input("Enter the location of the file in folder head! "), "D":input("Enter the location of the file in folder default! ")}
+src = rf"{src}"
 def replace():
     mod = input("Press D,H and A for Default, Head Mod, Antena Mod respectively: ")
-    if mod == "H":
-        os.replace(mods[0], srcfile)
-        print("replaced succesfully")
-    elif mod ==  "A":
-        os.replace(mods[1], srcfile)
-        print("replaced successfuly")
-    elif mod == "D":
-        os.replace(mods[2], srcfile)
+    shutil.copyfile(rf"{mods[mod]}", src)
+    print("replaced succesfully")
 while True:
     try:
         replace()
-    except Exception:
+    except Exception as e:
         print("Cannot Replace")
+        print(e)
