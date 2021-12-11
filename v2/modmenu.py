68d6cb1c2d9eda1f5f68dcac1c8dbcba90dc2965
living Gamer <keshabpaul45@gmail.com>
2021-12-11 18:07:13 +0530

    Restructured Everything

--- v2/modmenu.py
+++ v2/modmenu.py
@@ -1,15 +1,48 @@
+import kivy
 from kivy.app import App
+from kivy.core import text
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
 from kivy.uix.widget import Widget
-from kivy.lang import Builder
+from kivy.core.window import Window
 
-Builder.load_file('menu.kv')
-class MyLayout(Widget):
-	pass
-	
-class AwesomeApp(App):
+Window.size = (400, 600)
+
+class window_widget(Widget):
+	def __init__(self, **kwargs):
+		super(window_widget, self).__init__(**kwargs)
+
+		reload = Button(text ='RELOAD',
+				font_size ="15sp",
+				background_color =(1, 1, 1, 1),
+				color =(1, 1, 1, 1),
+				size=(250,175),
+				pos=(75,400))
+		reload.bind(on_press = self.rehack)
+		self.add_widget(reload)
+
+		hckdir = TextInput(hint_text ='Enter the path for Asset Indexer',
+				font_size ="15sp",
+                size=(175, 40),
+                pos=(75, 375))
+		self.add_widget(hckdir)
+
+		brwse = Button(text ='Browse',
+				font_size ="15sp",
+				background_color =(1, 1, 1, 1),
+				color =(1, 1, 1, 1),
+				size=(75, 40),
+				pos=(250, 375))
+		self.add_widget(brwse)
+	def rehack(self, instance):
+		print("Button is pressed")
+
+
+# create App class
+class modmenu(App):
+
 	def build(self):
-		return MyLayout()
+		return window_widget()
 
-
-if __name__ == '__main__':
-	AwesomeApp().run()
+if __name__ == "__main__":
+	modmenu().run()
